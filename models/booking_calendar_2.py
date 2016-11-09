5615bb41b863566c70401f0170ffff44a7d7a589
doc-tober-moon <doc.tober.moon@gmail.com>
2016-11-09 15:34:33 -0500

    V2 checkin

--- models/booking_calendar_2.py
+++ models/booking_calendar_2.py
@@ -1,67 +1,119 @@
+import pytz
 import calendar
+import datetime
 
+from bson.objectid import ObjectId
 from utils import to_display_month
-from settings import availability_coll
+from settings import availability_coll as coll
 from models.base_document import BaseDocument
+from models.coach import Coach
 
 # The maximum number of appointments per day that a coach can handle
 MAX_SLOTS = 8
 
+ONE_DAY = datetime.timedelta(days=1)
+
 class CalendarDay(object):
     '''Represents a single day of a coach's calendar'''
 
-    def __init__(self, coach_id, year, month, day):
+    def __init__(self, coach, date, _id=None, slots=None, locked=False, appt_ct=0):
 
-        query = {
-            'coach_id': coach_id,
-            'year': year,
-            'month': month,
-            'day': day
+        # If this object was not prev in db, there will be no id
+        if _id is None:
+            slots = [None] * MAX_SLOTS
+            res = coll.insert_one({
+                'coach': coach,
+                'date': date,
+                'slots': slots,
+                'locked': locked,
+                'appt_ct': appt_ct
+            })
+            _id = res.inserted_id
+
+
+        self.date = date
+        self._id = _id
+        self.coach = coach
+        self.slots = slots
+        self.appt_ct = appt_ct
+
+        # Store this for convienence
+        self.timezone = Coach(coach)['tzname']
+
+    def update_state(self, data):
+        '''Updates this object with possible new data from the db
+
+        The only values that may change are the slots and the appointment
+        count (we don't care about the locked state in this object)
+        '''
+        if 'slots' in data:
+            self.slots = data['slots']
+        if 'appt_ct' in data:
+            self.appt_ct = data['appt_ct']
+
+    def to_dict():
+        return {
+            '_id': self._id,
+            'coach': self.coach,
+            'date': self.date,
+            'slots': self.slots,
+            'locked': self.locked,
+            'appt_ct': self.appt_ct
         }
 
-        # Pull any matching object from the database
-        data = availability_coll.find_one(query)
-
-        # If no matching object, create a default object
-        if not data:
-            data = self.create_day(coach_id, year, month, day)
-
-        self._id = data.get('_id')
-        self.year     = year
-        self.month    = month
-        self.day      = day
-        self.coach_id = coach_id
-        self.slots    = data['slots']
-        self.appointments = data['appointments']
+    @property
+    def year(self):
+        return self.date.year
 
     @property
-    def available(self):
-        return self.appointments != MAX_SLOTS
+    def month(self):
+        return self.date.month
+
+    @property
+    def day(self):
+        return self.date.day
+
+    def display_times_as_timezone(self, tzname):
+        '''Converts the times of each slot to a display string in the specified timezone'''
+        for i in range(8):
+            yield self.display_time_for_slot(tzname, i)
+
+    def display_time_for_slot(self, tzname, slot):
+        coach_time = self.get_slot_time(slot)
+        return coach_time.astimezone(pytz.timezone(tzname)).strftime('%-I %p %Z')
+
+    def get_slot_time(self, slot):
+        tz = self.timezone
+        d = self.date
+
+        conv = datetime.datetime(
+                year=d.year, month=d.month, day=d.day, hour=9+slot,
+                minute=0, second=0
+
+        )
+        conv = pytz.timezone(tz).localize(conv)
+        print('Slottime')
+        print(conv)
+        return conv
+
+    def available(self, slot):
+        slot_time = self.get_slot_time(slot)
+        now = datetime.datetime.now(tz=pytz.utc)
+        return now < slot_time and self.slots[slot] is None
+
+    def booked_by(self, slot, user):
+        return self.slots[slot] == user
+
+    def display_time(self, slot):
+        return self.get_slot_time(slot)
 
     @property
     def display_date(self):
-        monthname = to_display_month(self.month)
-        return '{m} {d}'.format(m=monthname, d=self.day)
+        return self.date.strftime('%a, %b %d')
 
-    def create_day(self, coach_id, year, month, day):
-
-        #month_start, num_days = calendar.monthrange(year, month)
-
-        _id = '%s_%s_%s_%s' % (coach_id, year, month, day)
-        print(_id)
-        data = {
-            '_id': _id,
-            'year': year,
-            'month': month,
-            'day': day,
-            'coach_id': coach_id,
-            'slots': [None] * 8,
-            'locked': False,
-            'appointments': 0
-
-        }
-        availability_coll.insert(data)
-        return data
+    def _get_query(self):
+        '''Returns the query that can be used to retrieve db data'''
+        return {'coach': self.coach, 'date': self.date}
 
     def book(self, user_id, slot):
         '''Attempts to reserve a spot on a coach's calendar.
@@ -69,58 +121,100 @@ class CalendarDay(object):
         Before an appointment slot is taken, the object is locked to prevent
         concurrent modification.
         '''
-        query = {'_id': self._id}
-        data = self.lock_and_query()
+        success = True
+        # Only returns when the document lock has been acquired
+        query = self._get_query()
+        data = self.lock_and_query(query)
 
         # Update our current state
-        self.slots = data['slots']
-        self.appointments = data['appointments']
+        self.update_state(data)
 
         if self.slots[slot] is None:
             self.slots[slot] = user_id
-            self.appointments += 1
-            field = 'slots.%d' % slot
-            # Update the field directly in the database
+            self.appt_ct += 1
+            slot_idx = 'slots.%d' % slot
+
+            # Update the db object, unlock
             update = {
                 '$set': {
-                    field: user_id,
+                    slot_idx : user_id,
                     'locked': False,
-                    'appointments': self.appointments
-                }
+                },
+                '$inc': {'appt_ct': 1}
             }
-            availability_coll.update_one(query, update)
-            return True
-        else:
-            # Release the lock with no change
-            update = {'$set': {'locked': False}}
-            availability_coll.update_one(query, update)
-            return False
 
-    def unbook(self, user_id, slot):
+        else:
+            # Someone else got here first - release the lock with no change
+            success = False
+            update = {
+                    '$set': {
+                        'locked': False
+                        }
+                    }
+        coll.update_one(query, update)
+
+    def unbook(self, slot):
         '''Removes a user from a coach's calendar'''
 
-        self.appointments -= 1
-
         # The specific position in the slot array to unbook
-        field  = 'slots.%d' % slot
-        query  = {'_id': self._id}
-        update = {'$unset': {field:None}, 'appointments': self.appointments}
+        slot_idx = 'slots.%d' % slot
+        query = self._get_query()
+        update = {'$unset': {slot_idx:None}, '$inc': {'appt_ct': -1}}
 
         # Should be no reason no lock the database here
-        availability_coll.find_one_and_update(query, update)
+        data = coll.find_one_and_update(query, update)
+        self.update_state(data)
+
         return True
 
-    def lock_and_query(self):
+    def lock_and_query(self, query):
         '''Uses an atomic update to lock the document and return the current
         state of the document from the db'''
-        query = {'_id': self._id}
+        retries_left = 5
         op    = {'$set': {'locked': True}}
 
-        data = availability_coll.find_one_and_update(query, op)
+        data = coll.find_one_and_update(query, op)
 
         # If the data is locked, try again
         while data['locked'] is True:
-            data = availability_coll.find_one_and_update(query, op)
+            data = coll.find_one_and_update(query, op)
+            retries_left -= 1
+
+            if retries_left is 0:
+                raise IOError('Could not get object lock')
+
         return data
+
+    @staticmethod
+    def all_availability_in_range(_from, to, coach):
+        '''Gets a range of availability from the from date (inclusive) to the to date.  '''
+
+        # Due to a limitation in the mongodb driver, instead of using the more intuitive 'date'
+        # object, we have to use a datetime with hours, min, secs set to 0
+        _from = datetime.datetime(year=_from.year, month=_from.month, day=_from.day)
+        to = datetime.datetime(year=to.year, month=to.month, day=to.day)
+        print(_from)
+        print(to)
+
+        availabilities = []
+        while _from < to:
+            query = { 'date': {'$eq' : _from }}
+            data  = coll.find_one(query, projection={'coach': 0, 'date': 0})
+            if not data:
+                data = {}
+            availabilities.append(CalendarDay(coach, _from, **data))
+            _from += ONE_DAY
+
+        return availabilities
+
+    @staticmethod
+    def lookup_by_id(_id):
+        data = coll.find_one({'_id': ObjectId(_id)})
+        if data:
+            return CalendarDay(data['coach'], data['date'], _id=data['_id'], slots=data['slots'],
+                    locked=data['slots'], appt_ct=data['appt_ct'])
+
+
+
 
 
--- scheduler.py
+++ scheduler.py
@@ -16,6 +16,8 @@ from models.customer import Customer
 
 app = Flask(__name__)
 
+# Useful constants
+ONE_WEEK = datetime.timedelta(days=7)
 
 # Some systems / redirects that I assume would exist
 # If the user is unrecognized, redirect them to a signup page
@@ -33,74 +35,50 @@ def help():
 def timezone_help():
     return render_template('timezone.html')
 
-def get_availability(coach_id, customer_time, week_offset):
+def get_availability_range(startdate, enddate):
+    '''
+    Gets availability between two dates
+    '''
+    # If the range spans multiple months
+    pass
+
+
+
+def get_monthly_availability(coach, customer_time, week_offset):
+    '''
+    Gets availability for the whole month
+    '''
+    pass
+
+
+
+def get_weekly_availability(coach, customer_time, week_offset):
     '''
     customer time is current time in customer timezone
     take that time, set it to the beginning of this week, and add 7*weeks_offset
     to get the week that the customer has requested
     '''
 
-    appointments = []
     # todo see if customer's time is past coach's last time for today, aand if
     # this is the last day of the week - advance one week
-    customer_date = customer_time.date()
+    weekday = customer_time.weekday()
 
-    weekday = customer_date.weekday()
-    delta = datetime.timedelta(days=(-1 *weekday) + (7*week_offset))
-    beginning_of_week = customer_date + delta
-    ptr = beginning_of_week
-    one_day = datetime.timedelta(days=1)
-    for i in range(7):
-        appointments.append(CalendarDay(coach_id, ptr.year, ptr.month, ptr.day))
-        ptr += one_day
-    return appointments
+    # This delta will create a datetime that's at the beginning of week N
+    # Adjust by an additional day to display sunday-saturday
+    start_delta = datetime.timedelta(days=(-1 *weekday) - 1 + (7*week_offset))
 
-DISPLAY_TIMES = [
-    'Midnight',
-    '1 AM',
-    '2 AM',
-    '3 AM',
-    '4 AM',
-    '5 AM',
-    '6 AM',
-    '7 AM',
-    '8 AM',
-    '9 AM',
-    '10 AM',
-    '11 AM',
-    'Noon',
-    '1 PM',
-    '2 PM',
-    '3 PM',
-    '4 PM',
-    '5 PM',
-    '6 PM',
-    '7 PM',
-    '8 PM',
-    '9 PM',
-    '10 PM',
-    '11 PM',
-    'Midnight'
-    ]
-def get_display_times(cust_time, coach_time):
-    '''
-    Customer is reserving times in their own timezone.
-
-    Coach is available from 9-5 in their own timezone.
-
-    Bookable times are [9..5] + (customer timezone - coach timezone)
-    '''
-    offset = get_timezone_offset(cust_time, coach_time)
-    return DISPLAY_TIMES[9-offset:17-offset]
-
-def get_timezone_offset(t1, t2):
-    delta = t1 - t2
-    return int(delta.days * 24 + delta.seconds / 3600)
+    start = customer_time + start_delta
+    end = start + ONE_WEEK
+    return CalendarDay.all_availability_in_range(start, end, coach)
 
 
 
-@app.route('/appointment/<customer_id>/<int:offset>')
-def appointment_page(customer_id, offset):
+@app.route('/appt/<customerid>')
+def home(customerid):
+    return redirect(url_for('appointment_page', customerid=customerid, offset=0))
+
+@app.route('/appt/<customerid>/<int:offset>')
+def appointment_page(customerid, offset):
     '''
     This endpoint
     - look up coach for user
@@ -108,60 +86,39 @@ def appointment_page(customer_id, offset):
     - see if coach and user are in different timezones
     '''
 
-    customer = Customer(customer_id)
-    coach    = customer.get_coach()
+    customer = Customer(customerid)
+    coach = customer.get_coach()
     if not coach:
         return redirect(url_for('unrecognized_user'))
 
-    coachname = coach.display_name
-
-    customername = customer.display_name
-    customeremail = customer['email']
-
     now = time_utils.utcnow()
-    month = now.month
-    year = now.year
-
     customer_time = customer.to_timezone(now)
-    coach_time = coach.to_timezone(now)
-    hours = get_display_times(customer_time, coach_time)
-    days = get_availability(coach['email'], customer_time, offset)
 
-    tz_different = (coach['tzname'] != customer ['tzname'])
+    days = get_weekly_availability(coach['email'], customer_time, offset)
+
     return render_template(
         'reservation_base.html',
-        customername=customername,
-        customer_id=customer_id,
-        customeremail=customeremail,
-        coach_id=coach['email'],
-        coachname=coachname,
-        month=to_display_month(month),
-        year=year,
-        tz_different=tz_different,
-        hours=hours,
+        customer=customer,
+        coach=coach,
         days=days,
         offset=offset
     )
 
-@app.route('/book/<coach_id>/<customer_id>/<int:year>/<int:month>/<int:day>/<int:slot>')
-def book(coach_id, customer_id, year, month, day, slot):
-    print(year)
-    print(month)
-    print(day)
-    print(coach_id)
-    c = CalendarDay(coach_id, year, month, day)
-    c.book(customer_id, slot)
-    return redirect(url_for('index', customer_id=customer_id))
+@app.route('/book/<_id>/<customer>/<int:slot>')
+def book(_id, customer, slot):
+    c = CalendarDay.lookup_by_id(_id)
+    success = c.book(customer, slot)
+    if success:
+        return redirect(url_for('home', customerid=customer))
+    else:
+        # Would like to give the user better feedback in the future
+        return redirect(url_for('home', customerid=customer))
 
-@app.route('/unbook/<coach_id>/<customer_id>/<int:year>/<int:month>/<int:day>/<int:slot>')
-def unbook(coach_id, customer_id, year, month, day, slot):
-    print(year)
-    print(month)
-    print(day)
-    print(coach_id)
-    c = CalendarDay(coach_id, year, month, day)
-    c.unbook(customer_id, slot)
-    return redirect(url_for('index', customer_id=customer_id))
+
+@app.route('/unbook/<_id>/<customer>/<int:slot>')
+def unbook(_id, customer, slot):
+    success = CalendarDay.lookup_by_id(_id).unbook(slot)
+    return redirect(url_for('home', customerid=customer, error=True))
 
 if __name__ == '__main__':
     app.run(debug=True)
